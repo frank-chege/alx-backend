a8ac40b39580c1831f4024ef12c588fee3aa1306
frankrazzih <francischege602@gmail.com>
2024-06-10 20:27:29 +0300

    i18n

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 '''get the locale'''
 from flask import request
+from flask_babel import Babel
 
 app = __import__('1-app').app
+babel = __import__('1-app').babel
+@babel.localesselector
 def get_locale():
     '''gets the locale'''
     return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
