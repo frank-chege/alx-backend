39da379e05c83387475de3f9c482921b678d0041
frankrazzih <francischege602@gmail.com>
2024-06-10 18:00:26 +0300

    i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python3
-import importlib
 from flask_babel import Babel
+'''creating a flask app'''
 
-# Dynamically import the Flask app from the module 0-app
-module_name = '0-app'
-app_module = importlib.import_module(module_name)
-app = app_module.app
+app = __import__('0-app').app
 
 class Config:
     '''configure the app'''
     LANGUAGES = ["en", "fr"]
-
-    @staticmethod
-    def init_app(app):
+    def __init__(self, app) -> None:
         app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-        app.config['BABEL_SUPPORTED_LOCALES'] = Config.LANGUAGES
+        app.config['BABEL_SUPPORTED_LOCALES'] = self.LANGUAGES
         app.config['DEFAULT_TIMEZONE'] = 'UTC'
 
-# Apply the configuration to the app
-Config.init_app(app)
-
-# Instantiate the Babel object
+Config(app)
 babel = Babel(app)
 
 if __name__ == '__main__':
